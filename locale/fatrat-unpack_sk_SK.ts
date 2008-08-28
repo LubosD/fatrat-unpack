d408f2ae978851d5457e76c82531116784b70b74
Lubos Dolezel <lubos@dolezel.info>
2008-08-28 18:19:46 +0200

    Adding a Slovak translation

---
+++ locale/fatrat-unpack_sk_SK.ts
@@ -0,0 +1,162 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="cs">
+<defaultcodec></defaultcodec>
+<context>
+    <name>FilesDlg</name>
+    <message>
+        <location filename="../ui_FilesDlg.h" line="68"/>
+        <source>Unpack an archive</source>
+        <translation>Rozbaliť archív</translation>
+    </message>
+    <message>
+        <location filename="../ui_FilesDlg.h" line="69"/>
+        <source>The following supported archives were found:</source>
+        <translation>Boli nájdené nasledujúce podporované archívy:</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordDlg</name>
+    <message>
+        <location filename="../ui_PasswordDlg.h" line="118"/>
+        <source>Password</source>
+        <translation>Heslo</translation>
+    </message>
+    <message>
+        <location filename="../ui_PasswordDlg.h" line="119"/>
+        <source>The archive is password protected. Please enter the password.</source>
+        <translation>Archív je chránený heslom. Prosím, zadajte heslo.</translation>
+    </message>
+    <message>
+        <location filename="../ui_PasswordDlg.h" line="120"/>
+        <source>Transfer&apos;s comment</source>
+        <translation>Komentár prenosu</translation>
+    </message>
+    <message>
+        <location filename="../ui_PasswordDlg.h" line="121"/>
+        <source>Archive&apos;s comment</source>
+        <translation>Komentár archívu</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressWidget</name>
+    <message>
+        <location filename="../ui_ProgressWidget.h" line="63"/>
+        <source>Form</source>
+        <translation>Formulár</translation>
+    </message>
+    <message>
+        <location filename="../ui_ProgressWidget.h" line="66"/>
+        <source>Restore</source>
+        <translation>Obnoviť</translation>
+    </message>
+    <message>
+        <location filename="../ui_ProgressWidget.h" line="69"/>
+        <source>+</source>
+        <translation>+</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../fr_unpack.cpp" line="47"/>
+        <source>Unpack archive(s)...</source>
+        <translation>Rozbaliť archív(y)...</translation>
+    </message>
+    <message>
+        <location filename="../fr_unpack.cpp" line="57"/>
+        <source>Unpack plugin</source>
+        <translation>Unpack plugin</translation>
+    </message>
+    <message>
+        <location filename="../Unpacker.cpp" line="64"/>
+        <source>Unpack an archive</source>
+        <translation>Rozbaliť archív</translation>
+    </message>
+    <message>
+        <location filename="../fr_unpack.cpp" line="87"/>
+        <source>This file is not a supported archive!</source>
+        <translation>Tento súbor nie je podporovaným archívom!</translation>
+    </message>
+    <message>
+        <location filename="../fr_unpack.cpp" line="98"/>
+        <source>No supported archives found.</source>
+        <translation>Nenájdené žiadne podporované archívy.</translation>
+    </message>
+</context>
+<context>
+    <name>RarUnpacker</name>
+    <message>
+        <location filename="../RarUnpacker.cpp" line="230"/>
+        <source>Unable to open the archive.</source>
+        <translation>Nemôžem otvoriť archív.</translation>
+    </message>
+    <message>
+        <location filename="../RarUnpacker.cpp" line="112"/>
+        <source>The archive is corrupt.</source>
+        <translation>Archív je poškodený.</translation>
+    </message>
+    <message>
+        <location filename="../RarUnpacker.cpp" line="267"/>
+        <source>Cannot read the archive: %1.</source>
+        <translation>Nemôžem načítať archív: %1.</translation>
+    </message>
+    <message>
+        <location filename="../RarUnpacker.cpp" line="200"/>
+        <source>Volume %1 not found</source>
+        <translation>Zväzok %1 nenájdený</translation>
+    </message>
+    <message>
+        <location filename="../RarUnpacker.cpp" line="254"/>
+        <source>Unable to open %1 for writing.</source>
+        <translation>Nemôžem otvoriť %1 pre zápis.</translation>
+    </message>
+    <message>
+        <location filename="../RarUnpacker.cpp" line="256"/>
+        <source>Extracting %1...</source>
+        <translation>Rozbaľujem %1...</translation>
+    </message>
+    <message>
+        <location filename="../RarUnpacker.cpp" line="287"/>
+        <source>Unpack an archive</source>
+        <translation>Rozbaľujem archiv</translation>
+    </message>
+</context>
+<context>
+    <name>UnpackDlg</name>
+    <message>
+        <location filename="../UnpackDlg.ui" line="13"/>
+        <source>Unpack an archive</source>
+        <translation>Rozbaliť archív</translation>
+    </message>
+    <message>
+        <location filename="../UnpackDlg.ui" line="26"/>
+        <source>Unpack</source>
+        <translation>Rozbaliť</translation>
+    </message>
+    <message>
+        <location filename="../UnpackDlg.ui" line="33"/>
+        <source>Cancel</source>
+        <translation>Zrušiť</translation>
+    </message>
+    <message>
+        <location filename="../UnpackDlg.ui" line="40"/>
+        <source>Hide</source>
+        <translation>Skryť</translation>
+    </message>
+    <message>
+        <location filename="../UnpackDlg.cpp" line="30"/>
+        <source>Name</source>
+        <translation>Názov</translation>
+    </message>
+    <message>
+        <location filename="../UnpackDlg.cpp" line="31"/>
+        <source>Size</source>
+        <translation>Veľkosť</translation>
+    </message>
+    <message>
+        <location filename="../UnpackDlg.cpp" line="32"/>
+        <source>Progress</source>
+        <translation>Priebeh</translation>
+    </message>
+</context>
+</TS>
