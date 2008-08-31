3586b5ea0bd8aed98672368c42bebcdaca330f4b
Lubos Dolezel <lubos@dolezel.info>
2008-08-31 14:05:40 +0200

    Adding a Turkish translation

---
+++ locale/fatrat-unpack_tr_TR.ts
@@ -0,0 +1,133 @@
+<!DOCTYPE TS>
+<TS>
+  <context>
+    <name>FilesDlg</name>
+    <message>
+      <source>Unpack an archive</source>
+      <translation>Bir arşivi aç</translation>
+    </message>
+    <message>
+      <source>The following supported archives were found:</source>
+      <translation>Şu desteklenen arşivler bulundu:</translation>
+    </message>
+  </context>
+  <context>
+    <name>PasswordDlg</name>
+    <message>
+      <source>Password</source>
+      <translation>Şifre</translation>
+    </message>
+    <message>
+      <source>The archive is password protected. Please enter the password.</source>
+      <translation>Arşiv şifre korumalı. Lütfen şifreyi giriniz.</translation>
+    </message>
+    <message>
+      <source>Transfer's comment</source>
+      <translation>Transferin yorumu</translation>
+    </message>
+    <message>
+      <source>Archive's comment</source>
+      <translation>Arşivin yorumu</translation>
+    </message>
+  </context>
+  <context>
+    <name>ProgressWidget</name>
+    <message>
+      <source>Form</source>
+      <translation>Form</translation>
+    </message>
+    <message>
+      <source>Restore</source>
+      <translation>Geri Yükleme</translation>
+    </message>
+    <message>
+      <source>+</source>
+      <translation>+</translation>
+    </message>
+  </context>
+  <context>
+    <name>QObject</name>
+    <message>
+      <source>Unpack archive(s)...</source>
+      <translation>Arşiv(ler)i aç...</translation>
+    </message>
+    <message>
+      <source>Unpack plugin</source>
+      <translation>Açma eklentisi</translation>
+    </message>
+    <message>
+      <source>Unpack an archive</source>
+      <translation>Bir arşivi aç</translation>
+    </message>
+    <message>
+      <source>This file is not a supported archive!</source>
+      <translation>Dosya desteklenen bir arşiv değil!</translation>
+    </message>
+    <message>
+      <source>No supported archives found.</source>
+      <translation>Desteklenen arşivler bulunamadı.</translation>
+    </message>
+  </context>
+  <context>
+    <name>RarUnpacker</name>
+    <message>
+      <source>Unable to open the archive.</source>
+      <translation>Arşiv açılamadı.</translation>
+    </message>
+    <message>
+      <source>The archive is corrupt.</source>
+      <translation>Arşiv bozuk.</translation>
+    </message>
+    <message>
+      <source>Cannot read the archive: %1.</source>
+      <translation>Arşiv okunamadı: %1.</translation>
+    </message>
+    <message>
+      <source>Volume %1 not found</source>
+      <translation>Yığın %1 bulunamadı</translation>
+    </message>
+    <message>
+      <source>Unable to open %1 for writing.</source>
+      <translation>%1 yazma için açılamadı.</translation>
+    </message>
+    <message>
+      <source>Extracting %1...</source>
+      <translation>Çıkarılıyor %1...</translation>
+    </message>
+    <message>
+      <source>Unpack an archive</source>
+      <translation>Bir arşivi aç</translation>
+    </message>
+  </context>
+  <context>
+    <name>UnpackDlg</name>
+    <message>
+      <source>Unpack an archive</source>
+      <translation>Bir arşivi aç</translation>
+    </message>
+    <message>
+      <source>Unpack</source>
+      <translation>Aç</translation>
+    </message>
+    <message>
+      <source>Cancel</source>
+      <translation>İptal</translation>
+    </message>
+    <message>
+      <source>Hide</source>
+      <translation>Gizle</translation>
+    </message>
+    <message>
+      <source>Name</source>
+      <translation>Ad</translation>
+    </message>
+    <message>
+      <source>Size</source>
+      <translation>Boyut</translation>
+    </message>
+    <message>
+      <source>Progress</source>
+      <translation>İlerleme</translation>
+    </message>
+  </context>
+</TS>
